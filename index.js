4cf112250d826003126dd072984db2cae7ad26f5
Todd Carlson <todd.carlson3030@me.com>
2019-11-22 19:25:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  return appendkitten(name);
+  return appendKitten(name);
 }
